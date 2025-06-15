fe59bbffd2bf49c858d2d8634350568f684d421d
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-15 13:56:25 -0400

    update helpers

--- Helpers.cmake
+++ Helpers.cmake
@@ -264,3 +264,57 @@ macro(ecm_find_package_handle_library_components module_name)
         list(REMOVE_DUPLICATES ${module_name}_TARGETS)
     endif()
 endmacro()
+
+#################################################################################
+## END ECMFindModuleHelpers.cmake
+#################################################################################
+
+function(run_checks)
+    # We only support Linux. Everything else can go kick rocks.
+    if(NOT UNIX OR APPLE)
+        message(FATAL_ERROR "This operating system is not supported by ${PROJECT_NAME}.")
+    endif()
+endfunction()
+
+function(create_target)
+    set(SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/Source")
+    file(GLOB SOURCES "${SOURCE_DIR}/*.c" "${SOURCE_DIR}/Targets/*.c")
+
+    # Copy the public interface into the build directory.
+    file(COPY_FILE "${CMAKE_CURRENT_SOURCE_DIR}/Include/${PROJECT_NAME}.h" 
+        "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.h")
+    include_directories("${CMAKE_CURRENT_BINARY_DIR}")
+
+    add_library(${PROJECT_NAME} ${SOURCES})
+    set_target_properties(${PROJECT_NAME} PROPERTIES 
+        VERSION ${PROJECT_VERSION}
+        SOVERSION ${PROJECT_VERSION_MAJOR}
+        PUBLIC_HEADER "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.h"
+        C_STANDARD 23
+        C_STANDARD_REQUIRED ON
+    )
+    target_include_directories(${PROJECT_NAME} PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/Include")
+
+    target_compile_options(${PROJECT_NAME} PUBLIC -Wall -Werror -Wpedantic -Wextra)
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+        # https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html
+        set_target_properties(${PROJECT_NAME} PROPERTIES EXPORT_COMPILE_COMMANDS ON)
+
+        target_compile_options(${PROJECT_NAME} PUBLIC -Og -g3 -ggdb -fsanitize=address 
+            -fsanitize=pointer-compare  -fsanitize=leak -fsanitize=pointer-subtract 
+            -fsanitize=undefined)
+        target_link_options(${PROJECT_NAME} PUBLIC -fsanitize=address 
+            -fsanitize=undefined)
+
+        if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+            # https://gcc.gnu.org/onlinedocs/gcc/Static-Analyzer-Options.html
+            target_compile_options(${PROJECT_NAME} PUBLIC -fanalyzer)
+        endif()
+    else()
+        # https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html#index-march-15
+        # https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html#index-mtune-17
+        target_compile_options(${PROJECT_NAME} PUBLIC -march=native -mtune=native 
+            -Ofast -flto)
+        target_link_options(${PROJECT_NAME} PUBLIC -Ofast -flto)
+    endif()
+endfunction()
